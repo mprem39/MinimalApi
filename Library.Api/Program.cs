8f74a229eb8b0fdde4bcdde9c0c9d7dde0e36a7f
Prem <mprem39@gmail.com>
2026-09-05 13:19:08 +0530

    Implemented CRUD for Books Api with Dapper

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -1,4 +1,8 @@
+using FluentValidation;
+using FluentValidation.Results;
 using Library.Api.Data;
+using Library.Api.Models;
+using Library.Api.Services;
 using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +17,8 @@ builder.Services.AddSingleton<IDbConnectionFactory>(sp =>
     return new SqliteConnectionFactory(connectionString);
 });
 builder.Services.AddSingleton<DatabaseInitializer>();
+builder.Services.AddSingleton<IBookService, BookService>();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 var app = builder.Build();
 
@@ -22,6 +28,63 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapPost("books", async (Book book, IBookService bookService,
+    IValidator<Book> validator) =>
+{
+    var validationResult = await validator.ValidateAsync(book);
+    if (!validationResult.IsValid)
+    {
+        return Results.BadRequest(validationResult.Errors);
+    }
+
+    var created = await bookService.CreateAsync(book);
+    if (!created)
+    {
+        return Results.BadRequest(new List<ValidationFailure>
+        {
+            new ("Isbn", "A book with this ISBN-13 already exists")
+        });
+    }
+
+    return Results.Created($"/books/{book.Isbn}", book);
+});
+app.MapGet("books", async (IBookService bookService, string? searchTerm) =>
+{
+    if (searchTerm is not null && !string.IsNullOrWhiteSpace(searchTerm))
+    {
+        var matchedBooks = await bookService.SearchByTitleAsync(searchTerm);
+        return Results.Ok(matchedBooks);
+    }
+
+    var books = await bookService.GetAllAsync();
+    return Results.Ok(books);
+});
+
+app.MapGet("books/{isbn}", async (string isbn, IBookService bookService) =>
+{
+    var book = await bookService.GetByIsbnAsync(isbn);
+    return book is not null ? Results.Ok(book) : Results.NotFound();
+});
+
+app.MapPut("books/{isbn}", async (string isbn, Book book, IBookService bookService,
+    IValidator<Book> validator) =>
+{
+    book.Isbn = isbn;
+    var validationResult = await validator.ValidateAsync(book);
+    if (!validationResult.IsValid)
+    {
+        return Results.BadRequest(validationResult.Errors);
+    }
+
+    var updated = await bookService.UpdateAsync(book);
+    return updated ? Results.Ok(book) : Results.NotFound();
+});
+
+app.MapDelete("books/{isbn}", async (string isbn, IBookService bookService) =>
+{
+    var deleted = await bookService.DeleteAsync(isbn);
+    return deleted ? Results.NoContent() : Results.NotFound();
+});
 //DB init 
 var dbInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
 await dbInitializer.InitializeAsync();
